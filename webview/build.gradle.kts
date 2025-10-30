23d1ae18124865594cb3a5c174998b635d52c901
REP <16684132846@163.com>
2025-10-30 20:52:37 +0800

    fix:修复Android AAR 文件生成

--- webview/build.gradle.kts
+++ webview/build.gradle.kts
@@ -113,7 +113,7 @@ android {
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 24
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
